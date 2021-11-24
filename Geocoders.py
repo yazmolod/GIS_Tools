cbdb6ffc52d816130d0250b3bdcca6d208182118
Alexander Litovchenko <yazmolod@gmail.com>
2021-11-24 14:04:28 +0400

    pkk poly and point refactor

--- Geocoders.py
+++ Geocoders.py
@@ -1,16 +1,26 @@
-import time
-import requests
-from shapely.geometry import MultiPoint, Point
-from pyproj import Transformer
-from itertools import chain
 if __name__ == '__main__':
+    import FastLogging
     from ProxyGrabber import ProxyGrabber
 else:
     from .ProxyGrabber import ProxyGrabber
+
+import warnings
+from rosreestr2coord import Area, VERSION
+from rosreestr2coord.parser import TYPES
+if 'yazmo' not in VERSION:
+    warnings.warn("Use yazmolod/rosreestr2coord instead default rendrom/rosreestr2coord. It's too more safe and cooler", ImportWarning, stacklevel=2)
+
+import time
+import requests
+from shapely.geometry import MultiPoint, Point, shape
+from pathlib import Path
+import json
+from pyproj import Transformer
+from itertools import chain
 import re
 import logging
-
 logger = logging.getLogger(__name__)
+
 HERE_API_KEY = 'uqDak72P-C8FYyYkI4vz8EDxbNFhBJw4jW-fg9P4lb8'
 YANDEX_API_KEY = '3e976513-247e-4072-8b7b-2fd619d45b2c'
 
@@ -20,6 +30,12 @@ def get_grabber():
     if __GRABBER is None:
         __GRABBER = ProxyGrabber()
     return __GRABBER
+
+def _validate_kadastr(kadastr):
+    kad_check = re.findall(r'[\d:]+', kadastr)
+    if len(kad_check) != 1:
+        raise TypeError(f'"{kadastr}" не является поддерживаемым форматом для геокодирования')
+    return kad_check[0]
 
 def _rosreestr_request(current_kadastr):
     grabber = get_grabber()
@@ -36,7 +52,7 @@ def _rosreestr_request(current_kadastr):
     try:
         r = requests.get(url, proxies = grabber.get_proxy(), timeout=3)
     except:
-        logger.debug(f'(PKK) request exception [{current_kadastr}]')
+        logger.debug(f'(PKK Point) request exception [{current_kadastr}]')
         grabber.next_proxy()
         return _rosreestr_request(current_kadastr)
     if r.status_code != 200:
@@ -48,27 +64,26 @@ def _rosreestr_request(current_kadastr):
     if feature:
         center = feature.get('center')
         extent_parent = feature.get('extent_parent')
+        x = y = None
         if center:
             x,y = center['x'], center['y']
-            logger.info(f'(PKK) Kadastr geocoded [{current_kadastr}]')
+            logger.info(f'(PKK Point) Kadastr geocoded [{current_kadastr}]')
         elif extent_parent:
             x = (extent_parent['xmin'] + extent_parent['xmax'])/2
             y = (extent_parent['ymin'] + extent_parent['ymax'])/2
-            logger.info(f'(PKK) Kadastr geocoded by parent [{current_kadastr}]')
+            logger.info(f'(PKK Point) Kadastr geocoded by parent [{current_kadastr}]')
         if x and y:
             transformer = Transformer.from_crs("epsg:3857", "epsg:4326", always_xy=True)
             x, y = transformer.transform(x, y)
             return Point(x, y)          
         else:
-            logger.warning(f'(PKK) No center in feature [{current_kadastr}]')
+            logger.warning(f'(PKK Point) No center in feature [{current_kadastr}]')
     else:
-        logger.warning(f'(PKK) No features in response [{current_kadastr}]')
+        logger.warning(f'(PKK Point) No features in response [{current_kadastr}]')
 
-def rosreestr(kadastr, deep_search = False):
-    kad_check = re.findall(r'[\d:]+', kadastr)
-    if len(kad_check) != 1:
-        raise TypeError(f'"{kadastr}" не является поддерживаемым форматом для геокодирования')
-    kadastr = kad_check[0]
+def rosreestr(kadastr, deep_search=False):
+    warnings.warn('Method Geocoders.rosreestr is deprecated; use Geocoders.rosreestr_point', DeprecationWarning, stacklevel=2)
+    kadastr = _validate_kadastr(kadastr)
     if not deep_search:
         return _rosreestr_request(kadastr)
     else:
@@ -77,10 +92,34 @@ def rosreestr(kadastr, deep_search = False):
             current_kadastr = ':'.join(kad_numbers[: i + 1 ])
             pt = _rosreestr_request(current_kadastr)
             if pt:
-                return pt                
-        
+                return pt  
 
-def here(search_string, additional_params = None):
+KADASTR_TYPES = {k:v for k,v in TYPES.items() if k in ['Участки', 'ОКС']}
+def _rosreestr_geom(kadastr, center_only):
+    geom_type = 'Point' if center_only else 'Polygon'
+    kadastr = _validate_kadastr(kadastr)
+    for kadastr_type_alias, kadastr_type in KADASTR_TYPES.items():
+        logger.debug(f'(PKK Geom) Try geocode {kadastr} ({geom_type}), type {kadastr_type} ({kadastr_type_alias})')
+        area = Area(code=kadastr, area_type=kadastr_type, use_cache=True, center_only=center_only)
+        geom_method = area.to_geojson_center if center_only else area.to_geojson_poly
+        feature_string = geom_method()
+        if feature_string:
+            feature = json.loads(feature_string)
+            logger.info(f'(PKK Geom) Geocoded {kadastr} ({geom_type}), type {kadastr_type} ({kadastr_type_alias})')
+            geom = shape(feature['geometry'])
+            return geom
+        else:
+            logger.debug(f'(PKK Geom) Nothing found for {kadastr} ({geom_type}), type {kadastr_type} ({kadastr_type_alias})')
+            (Path(area.workspace) / "feature_info.json").unlink()   # удаляем кэш, так как был неверный тип 
+    logger.warning(f'(PKK Geom) {geom_type} not found ({kadastr})')
+
+def rosreestr_point(kadastr):
+    return _rosreestr_geom(kadastr, center_only=True)
+
+def rosreestr_polygon(kadastr):
+    return _rosreestr_geom(kadastr, center_only=False)
+
+def here(search_string, additional_params=None):
     # избавляемся от переносов строки, табуляции и прочего
     search_string = re.sub(r'\s+', ' ', search_string)
     # убираем повторяющиеся лексемы из запроса
@@ -105,7 +144,7 @@ def here(search_string, additional_params = None):
         r = requests.get(url)
         if r.status_code == 429:
             time.sleep(1)
-            return here(search_string, additional_params = None)
+            return here(search_string, additional_params=None)
         data = r.json()['items']
     except:
         logger.exception(f'(HERE) Error, status_code {r.status_code}')
@@ -122,7 +161,6 @@ def here(search_string, additional_params = None):
             return pt
         else:
             logger.warning(f'(HERE) Geocoder return empty list')
-
 
 def yandex(search_string):
     endpoint = 'https://geocode-maps.yandex.ru/1.x'
@@ -143,8 +181,8 @@ def yandex(search_string):
 
 
 if __name__ == '__main__':
-    import FastLogging
     logger = FastLogging.getLogger(__name__)
-    # pt = yandex('москва разина 1')
-    pt = rosreestr(' 22:8:11216:95', deep_search=True)
-    # logger.debug('test')
+    _1 = FastLogging.getLogger('GIS_Tools.ProxyGrabber')
+    _2 = FastLogging.getLogger('mylib.rosreestr2coord')
+    _3 = FastLogging.getLogger('rosreestr2coord')
+    pl = rosreestr_polygon('23:43:0106012:4122')
