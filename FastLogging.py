3efd70572ddb063991f5084ab89e1d3b8727452c
Alexander Litovchenko <yazmolod@gmail.com>
2023-11-28 19:30:12 +0400

    Update FastLogging.py

--- FastLogging.py
+++ FastLogging.py
@@ -6,7 +6,7 @@ STREAM_HANDLER.setLevel(logging.DEBUG)
 STREAM_HANDLER.setFormatter(FORMATTER)
 
 
-def getLogger(name, stream=True, file=None):
+def getLogger(name, stream=True, file=None, filemode='a+'):
 	"""быстро включает логгер, когда лень настраивать нормальный
 	"""
 	logger = logging.getLogger(name)
@@ -14,7 +14,7 @@ def getLogger(name, stream=True, file=None):
 	if stream:
 		logger.addHandler(STREAM_HANDLER)
 	if file:
-		FILE_HANDLER = logging.FileHandler(f'{file}', 'a+', encoding='utf-8')
+		FILE_HANDLER = logging.FileHandler(f'{file}', filemode, encoding='utf-8')
 		FILE_HANDLER.setLevel(logging.INFO)
 		FILE_HANDLER.setFormatter(FORMATTER)
 		logger.addHandler(FILE_HANDLER)
